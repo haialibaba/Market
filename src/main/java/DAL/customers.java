3360cdd3066bb1a6e7e70c8688d5e00f8cbcde17
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2023-03-31 17:13:22 +0700

    completed entity customer

--- src/main/java/DAL/customers.java
+++ src/main/java/DAL/customers.java
@@ -4,10 +4,35 @@
  */
 package DAL;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
 /**
  *
  * @author PC
  */
+
+@Data
+@Entity
+@Table
 public class customers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int CustomerID;
+    @Column
+    private String Password;
+    @Column
+    private String Fullname;
+    @Column
+    private String Address;
+    @Column
+    private String City;
+
     
 }
